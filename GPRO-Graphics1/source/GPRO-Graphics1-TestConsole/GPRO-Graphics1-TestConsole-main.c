091ad294dc14f8a6a2f7abc290d6b3e28fc4b5a3
steven.annunziato <steven.annunziato@mymail.champlain.edu>
2020-09-06 11:28:13 -0400

    Implemented chapter 2: output an image

--- GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.c
+++ GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.c
@@ -18,10 +18,13 @@
 	GPRO-Graphics1-TestConsole-main.c/.cpp
 	Main entry point source file for a Windows console application.
 
-	Modified by: ____________
+	Modified by: Steven Annunziato
 	Modified because: ____________
-*/
 
+	Modifications adapted from Peter Shirley's Ray Tracing in One Weekend.
+	https://raytracing.github.io/books/RayTracingInOneWeekend.html#overview
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -56,6 +59,33 @@ void testVector()
 int main(int const argc, char const* const argv[])
 {
 	testVector();
+
+	const int IMAGE_WIDTH = 256;
+	const int IMAGE_HEIGHT = 256;
+
+	// render image
+	// create ppm text file for the image
+	printf("P3\n%i %i\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+	// create rgb values per pixel
+	for (int i = IMAGE_HEIGHT - 1; i >= 0; --i) {
+		// print progress indicator to the error output stream
+		fprintf(stderr, "\rScanlines remaining: %i \n", i);
+		fflush(stderr);
+		for (int j = 0; j < IMAGE_WIDTH; ++j) {
+			double r = (double)j / (IMAGE_WIDTH - 1);
+			double g = (double)i / (IMAGE_HEIGHT - 1);
+			double b = 0.25;
+
+			int jr = (int)(255.999 * r);
+			int jg = (int)(255.999 * g);
+			int jb = (int)(255.999 * b);
+
+			printf("%i %i %i\n", jr, jg, jb);
+		}
+	}
+	fprintf(stderr, "\nDone.\n");
+
 
 	printf("\n\n");
 	system("pause");
